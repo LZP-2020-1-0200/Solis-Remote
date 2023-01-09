307bd35a20cd6c1dfacfb44ab9e5d46886786803
Scarpy019 <Scarpy019@gmail.com>
2023-01-09 11:59:37 +0200

    Minor refactor of translator.py

--- translator.py
+++ translator.py
@@ -14,7 +14,8 @@ SystemB:list[Coordinate]=[
 while True:
     print("Select mode\n1-from A to B\n2-from B to A\n3-exit")
     mode:int =int(input("Enter mode: "))
-    if mode==3:break
+    if mode==3:
+        break
     if mode==1 or mode==2:
         x:float=float(input("Enter X coordinate: "))
         y:float=float(input("Enter Y coordinate: "))
@@ -23,6 +24,3 @@ while True:
             print(translator.anchor_translate(SystemB,SystemA,c))
         if mode==2:
             print(translator.anchor_translate(SystemA,SystemB,c))
-
-
-
